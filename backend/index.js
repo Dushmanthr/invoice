c55ad54d331ef1c59e4e16b54a6878a7619840e2
Dushmantha Ranathunga <e102.mavicsoft@gmail.com>
2024-04-29 01:22:24 +0530

    fix:fix mongo db connection issue

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,7 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB Connected...'))
   .catch((err) => console.error(err));
+  //process.exit(1);
 
 app.use('/invoices', invoicesRouter);
 
